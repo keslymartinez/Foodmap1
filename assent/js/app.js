c97ec7ce9cde936ca3aab2633fe2e26a82b72faf
keslymartinez <keslymartinezvaldes@gmail.com>
2017-12-14 09:47:12 -0300

    subiendo archivo

---
+++ assent/js/app.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+   $('.container1').fadeOut(3300);
+});
+
+$(document).ready(function(){
+   $('.container2').delay(3100).fadeIn("slow");
+});
+
+
+$(document).ready(function(){
+
+	$('#miBuscador').on('change', function(){
+		var selectValor = '#'+$(this).val();
+		$('#pai').children('div').hide();
+		$('#pai').children(selectValor).show();
+	
+	});
+});
+
